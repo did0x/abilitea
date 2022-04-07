fdd615013ddb77e8d6440f7460604dad88badfde
Fabriano28 <ferumbromin@gmail.com>
2022-04-07 21:27:40 +0700

    Add temporary view, outlet, and action template

--- abilitea/ViewController.swift
+++ abilitea/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topicLabel: UILabel!
+    @IBOutlet weak var quizNumberLabel: UILabel!
+    @IBOutlet weak var quizImage: UIImageView!
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var optionABtn: UIButton!
+    @IBOutlet weak var optionBBtn: UIButton!
+    @IBOutlet weak var optionCBtn: UIButton!
+    @IBOutlet weak var optionDBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func selectOptionA(_ sender: Any) {
+    }
+    
+    @IBAction func selectOptionB(_ sender: Any) {
+    }
+    
+    @IBAction func selectOptionC(_ sender: Any) {
+    }
+    
+    @IBAction func selectOptionD(_ sender: Any) {
+    }
+    
+    @IBAction func selectNextQuestion(_ sender: Any) {
+    }
 }
 
