b18d48c8f92864c914ae97523f7ba7faa9235c3f
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-09 02:37:45 +0700

    Update Homepage

---
+++ abilitea/Controller/CognitiveModuleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CognitiveModuleCollectionViewCell.swift
+//  abilitea
+//
+//  Created by Rido Hendrawan on 08/04/22.
+//
+
+import UIKit
+
+class CognitiveModuleCollectionViewCell: UICollectionViewCell {
+    
+}
