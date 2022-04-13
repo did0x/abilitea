db1b59cdf7ae9031bb3d0d697daea447337d3874
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-13 23:54:04 +0700

    add selected multiple action for module cognitive

--- abilitea/Controller/TutorialTestController.swift
+++ abilitea/Controller/TutorialTestController.swift
@@ -9,7 +9,70 @@ import UIKit
 
 class TutorialTestController: UIViewController{
     /// Tutorial Test Outlets
-
+    
+    @IBOutlet weak var answer1: UIButton!
+    @IBOutlet weak var answer2: UIButton!
+    @IBOutlet weak var answer3: UIButton!
+    @IBOutlet weak var answer4: UIButton!
+    
+    @IBAction func answer1Action(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            answer2.isSelected = false
+            answer3.isSelected = false
+            answer4.isSelected = false
+        } else {
+            sender.isSelected = true
+            answer2.isSelected = false
+            answer3.isSelected = false
+            answer4.isSelected = false
+        }
+        
+    }
+    
+    @IBAction func answer2Action(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            answer1.isSelected = false
+            answer3.isSelected = false
+            answer4.isSelected = false
+        } else {
+            sender.isSelected = true
+            answer1.isSelected = false
+            answer3.isSelected = false
+            answer4.isSelected = false
+        }
+    }
+    
+    @IBAction func answer3Action(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            answer1.isSelected = false
+            answer2.isSelected = false
+            answer4.isSelected = false
+        } else {
+            sender.isSelected = true
+            answer1.isSelected = false
+            answer2.isSelected = false
+            answer4.isSelected = false
+        }
+    }
+    
+    @IBAction func answer4Action(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+            answer1.isSelected = false
+            answer2.isSelected = false
+            answer3.isSelected = false
+        } else {
+            sender.isSelected = true
+            answer1.isSelected = false
+            answer2.isSelected = false
+            answer4.isSelected = false
+        }
+    }
+    
+    
 
     @IBOutlet weak var navigationBarTutorial: UINavigationBar!
     @IBOutlet weak var questionImage: UIImageView!
