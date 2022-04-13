0eb5f611cd47f8b1d70a9bdb9ee92ca4ea041471
William-Yulio <william.yulio331@gmail.com>
2022-04-13 22:26:44 +0700

    Add Tutorial Test Page, Get To Know Page, and Congratulation Page

---
+++ abilitea/Controller/GetToKnowController.swift
@@ -0,0 +1,28 @@
+//
+//  GetToKnowController.swift
+//  abilitea
+//
+//  Created by William Yulio on 13/04/22.
+//
+
+import UIKit
+import SnapKit
+
+class GetToKnowController: UIViewController{
+    /// Start Tutorial Outlets
+    @IBOutlet weak var NavigationBarGetKnowFirst: UINavigationBar!
+    @IBOutlet weak var ImageViewStart: UIImageView!
+    @IBOutlet weak var CategoryLabelStart: UILabel!
+    @IBOutlet weak var TextViewStart: UITextView!
+    @IBOutlet weak var StartBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        
+    }
+    
+}
---
+++ abilitea/Controller/TutorialTestController.swift
@@ -0,0 +1,32 @@
+//
+//  TutorialTestController.swift
+//  abilitea
+//
+//  Created by William Yulio on 13/04/22.
+//
+
+import UIKit
+
+class TutorialTestController: UIViewController{
+    /// Tutorial Test Outlets
+
+
+    @IBOutlet weak var navigationBarTutorial: UINavigationBar!
+    @IBOutlet weak var questionImage: UIImageView!
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var buttonMultipleChoice1: UIButton!
+    @IBOutlet weak var buttonMultipleChoice2: UIButton!
+    @IBOutlet weak var buttonMultipleChoice3: UIButton!
+    @IBOutlet weak var buttonMultipleChoice4: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+    
+}
+
